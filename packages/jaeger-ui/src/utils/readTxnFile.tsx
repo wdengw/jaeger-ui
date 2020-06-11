858f81ee3ffcda9d09aa4e3575c8fd2fea784d5a
Weian Deng <wdeng@walmart.com>
2020-06-11 16:01:29 -0700

    First time sucessfully uploading transactions through Jaeger UI.

---
+++ packages/jaeger-ui/src/utils/readTxnFile.tsx
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export default function readTxnFile(txnFileList: { file: File }) {
+	const formData = new FormData()
+	formData.append('file', txnFileList.file)
+	formData.append('user-file', txnFileList.file)
+
+
+	const options = {
+		method: 'POST',
+		body: formData,
+		headers: {
+			'Content-Type': 'multipart/form-data',
+		}
+	}
+	delete options.headers['Content-Type'];
+
+	fetch('http://localhost:8080/upload-txn-file', options)
+	console.log("Done!")
+}
