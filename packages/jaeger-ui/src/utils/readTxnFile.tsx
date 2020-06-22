b115adcf9ad2018feff42c5faef56286cda4bbb3
Weian Deng <wdeng@walmart.com>
2020-06-22 13:08:22 -0700

    Transaction Uploads works now.

--- packages/jaeger-ui/src/utils/readTxnFile.tsx
+++ packages/jaeger-ui/src/utils/readTxnFile.tsx
@@ -1,16 +1,3 @@
-// Copyright (c) 2019 Uber Technologies, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 export default function readTxnFile(txnFileList: { file: File }) {
 	const formData = new FormData()
@@ -28,7 +15,7 @@ export default function readTxnFile(txnFileList: { file: File }) {
 	delete options.headers['Content-Type'];
     const  uri = process.env.REACT_APP_TXN_LOADER_URL
     if (uri !== undefined) {
-	    const url = uri + "/upload-txn-file"
+	    const url = uri + "/uploadTxnFile"
 		console.log("URL: " + url + ".")
 		fetch(url, options)
 			.then(response => response.text())
